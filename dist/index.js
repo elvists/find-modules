3a2b2b97c787f27d1b4fdfb80dd2146f5f2426c3
Elvis Teles <elvists@gmail.com>
2021-11-05 15:16:09 -0400

    chore: adjusting filter module

--- dist/index.js
+++ dist/index.js
@@ -1692,12 +1692,15 @@ const verifyHasFolder = (source, subfolder) =>
 
 try {
     const workingDirectory = core.getInput('working-directory');
+    const subfolderFilter = core.getInput('subfolder-filter');
 
     console.log(`Find modules in ${workingDirectory}`);
     var modules = getDirectories(workingDirectory);
-    if (core.getInput('subfolder') != null) {
-        modules = modules.filter(module => verifyHasFolder(`${workingDirectory}/${module}`, "test"));
+
+    if (subfolderFilter != null) {
+        modules = modules.filter(module => verifyHasFolder(`${workingDirectory}/${module}`, subfolderFilter));
     }
+
     console.log(`Modules list found: ${modules}`);
     core.setOutput("modules", modules);
 
